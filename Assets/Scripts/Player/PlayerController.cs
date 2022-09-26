d43fbebb4e1dc2db97ca65de860d51143b9f1685
Cristhian997 <rcagamez@uninorte.edu.co>
2022-09-26 13:43:15 -0500

    Beta 2.0

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,11 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     InputControll ip = new InputControll();
     Rigidbody2D rg;
+    Animator anim;
     public float vel;
+
+    bool isFacingRight = true;
+
     void Start()
     {
         rg = GetComponent<Rigidbody2D>();
-        
+        anim = GetComponent<Animator>();
+       
     }
 
     // Update is called once per frame
@@ -19,6 +24,38 @@ public class PlayerController : MonoBehaviour
         ip.check();
 
         rg.velocity = new Vector2 (ip.moveh * vel, ip.movev*vel);
+        
+        
+    }
+    private void FixedUpdate()
+    {
+        if (ip.moveh < 0 && isFacingRight)
+        {
+            Flip();
+        } else if (ip.moveh > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+    }
+    private void LateUpdate()
+    {
+        if (ip.moveh!=0 || ip.movev != 0) {
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
+    }
+    protected void Flip()
+    {
+        isFacingRight = !isFacingRight;
 
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+        
+        
     }
 }
--- Assets/Scripts/UI/ButtonController.cs
+++ Assets/Scripts/UI/ButtonController.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string name;
+    public void PassScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Game1");
     }
 }
